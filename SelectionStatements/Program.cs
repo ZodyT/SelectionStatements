f9790c6cfcd2c6b7a9136bc2cc227700fefa0d4a
ZodyT <Zoraida1988@yahoo.com>
2020-11-18 11:39:09 -0600

    Finished

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("What is my favoritve number?");
+            var userInput = int.Parse(Console.ReadLine());
+
+            bool Conditional = true;
+
+            while (Conditional)
+            {
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Your guess is too low");
+                    Console.WriteLine("Guess again");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Your guess is too high");
+                    Console.WriteLine("Guess again");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+
+                else if (userInput == favNumber)
+                {
+                    Console.WriteLine("Good Job!");
+                    Conditional = false;
+                }
+            }
+        }
+    }
+}
